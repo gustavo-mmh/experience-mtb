11b461796a78ad3ff460309d96c536933e815bf8
gustavo-mmh <gustavo-mmello@hotmail.com>
2025-02-03 14:50:34 -0300

    Adiciona configuração do Firebase e substituição de variáveis de ambiente no fluxo de deploy

--- assets/js/firebase/app.js
+++ assets/js/firebase/app.js
@@ -1,17 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-analytics.js";
-
-// Configuração do TESTE EXPERIENCE-MTB
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
+import firebaseConfig from './config.template.js';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
