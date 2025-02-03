d5acae5e6c9556c1e75d950e4592ebf86f29f0d0
gustavo-mmh <gustavo-mmello@hotmail.com>
2025-02-03 15:32:53 -0300

    Adiciona configuração do Firebase para o projeto Experience-MTB

--- assets/js/firebase/app.js
+++ assets/js/firebase/app.js
@@ -1,6 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-analytics.js";
-import firebaseConfig from './config.template.js';
+
+// Configuração do TESTE EXPERIENCE-MTB
+const firebaseConfig = {
+    apiKey: "FIREBASE_API_KEY_PLACEHOLDER",
+    authDomain: "FIREBASE_AUTH_DOMAIN_PLACEHOLDER",
+    projectId: "FIREBASE_PROJECT_ID_PLACEHOLDER",
+    storageBucket: "FIREBASE_STORAGE_BUCKET_PLACEHOLDER",
+    messagingSenderId: "FIREBASE_MESSAGING_SENDER_ID_PLACEHOLDER",
+    appId: "FIREBASE_APP_ID_PLACEHOLDER",
+    measurementId: "FIREBASE_MEASUREMENT_ID_PLACEHOLDER"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
